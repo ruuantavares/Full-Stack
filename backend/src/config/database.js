133080279c82c4456a4b615076f32afe048a1ea9
Ruan <ruuantavares@gmail.com>
2025-11-28 20:09:09 -0300

    feat: update database configuration to use PostgreSQL with correct credentials

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -8,11 +8,11 @@ class Database {
     init() {
         // .env - dotenv
         this.db = new Sequelize({
-            database: 'exemplo',
-            host: 'localhost',
-            username: 'root',
-            password: '',
-            dialect: 'mysql'
+            database: 'api-cadastro-usuario',
+            host: 'dpg-d4l285u3jp1c7394vjrg-a',
+            username: 'api_cadastro_usuario_user',
+            password: 'XK4LnDWJDvqbqW2h9PkqJAXsvUh6UCLW',
+            dialect: 'postgres'
         })
     }
 }

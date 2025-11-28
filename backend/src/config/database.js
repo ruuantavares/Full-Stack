1e93c77a5d556de553e35b2e428fed210545f7e6
Ruan <ruuantavares@gmail.com>
2025-11-28 20:38:15 -0300

    refactor: improve code formatting and consistency in database initialization

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,20 +1,27 @@
-import { Sequelize } from 'sequelize'
+import { Sequelize } from "sequelize";
 
 class Database {
-    constructor() {
-        this.init()
-    }
+  constructor() {
+    this.init();
+  }
 
-    init() {
-        // .env - dotenv
-        this.db = new Sequelize({
-            database: 'api-cadastro-usuario',
-            host: 'dpg-d4l285u3jp1c7394vjrg-a.oregon-postgres.render.com',
-            username: 'api_cadastro_usuario_user',
-            password: 'XK4LnDWJDvqbqW2h9PkqJAXsvUh6UCLW',
-            dialect: 'postgres'
-        })
-    }
+  init() {
+    // .env - dotenv
+    this.db = new Sequelize({
+      database: "api-cadastro-usuario",
+      host: "dpg-d4l285u3jp1c7394vjrg-a.oregon-postgres.render.com",
+      username: "api_cadastro_usuario_user",
+      password: "XK4LnDWJDvqbqW2h9PkqJAXsvUh6UCLW",
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      logging: false,
+    });
+  }
 }
 
-export default new Database()
+export default new Database();

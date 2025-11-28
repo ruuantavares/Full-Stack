97f76626b06de50da61912e894b7437ffa696308
Ruan <ruuantavares@gmail.com>
2025-11-28 20:43:15 -0300

    fix: correct database name format in Sequelize initialization

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -8,7 +8,7 @@ class Database {
   init() {
     // .env - dotenv
     this.db = new Sequelize({
-      database: "api-cadastro-usuario",
+      database: "api_cadastro_usuario",
       host: "dpg-d4l285u3jp1c7394vjrg-a.oregon-postgres.render.com",
       username: "api_cadastro_usuario_user",
       password: "XK4LnDWJDvqbqW2h9PkqJAXsvUh6UCLW",
